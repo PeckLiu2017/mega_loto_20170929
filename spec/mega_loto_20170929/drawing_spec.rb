88f37e8d484605f118846f62d6e94cd8c943eb50
peck liu <xiaoyang20170204@gmail.com>
2017-09-29 21:35:52 +0800

    try release

--- spec/mega_loto_20170929/drawing_spec.rb
+++ spec/mega_loto_20170929/drawing_spec.rb
@@ -1,26 +1,22 @@
 require 'spec_helper'
-RSpec.describe "home page" do
-# module MegaLoto20170929
-#   describe Drawing do
-    describe '#draw' do
-      let(:draw) { MegaLoto20170929::Drawing.new.draw }
-      it 'returns an array' do
-        expect(draw).to be_a(Array)
-      end
-      it 'returns an array with 6 elements' do
-        expect(draw.size).to eq(6)
-      end
-      it 'each element is an integer' do
-        draw.each do |drawing|
-          expect(drawing).to be_a(Integer)
-        end
-      end
-      it 'each element is less than 60' do
-        draw.each do |drawing|
-          expect(drawing).to be < 60
-        end
+RSpec.describe 'test' do
+  describe '#draw' do
+    let(:draw) { MegaLoto20170929::Drawing.new.draw }
+    it 'returns an array' do
+      expect(draw).to be_a(Array)
+    end
+    it 'returns an array with 6 elements' do
+      expect(draw.size).to eq(6)
+    end
+    it 'each element is an integer' do
+      draw.each do |drawing|
+        expect(drawing).to be_a(Integer)
       end
     end
-#   end
-# end
+    it 'each element is less than 60' do
+      draw.each do |drawing|
+        expect(drawing).to be < 60
+      end
+    end
+  end
 end
